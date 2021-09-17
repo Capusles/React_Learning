9a729fef4949264caf7a393ed25976547bb2b739
userlee <userlee@foxmail.com>
2021-09-17 23:16:04 +0800

    day02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render() {
+        return(
+            // JSX
+           <ul>
+               <li>{false?'Hello React':'哈喽React'}</li>
+               <li>React Learning</li>
+           </ul>
+        )
+        // var child1 = React.createElement('li',null,'Hello React')
+        // var child2 = React.createElement('li',null,'React Learning')
+        // var root = React.createElement('ul',{className:'my-list'},child1,child2)
+    }
 }
-
-export default App;
+export default App
---
+++ src/Xiaojiejie.js
@@ -0,0 +1,57 @@
+import React,{Component,Fragment} from "react";
+
+class Xiaojiejie extends Component{
+    constructor(props) {
+        super(props)
+        this.state={
+            inputValue:'',
+            list:['基础按摩','精油推背']
+        }
+    }
+    render() {
+        return(
+            <Fragment>
+                <div>
+                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)} /> 
+                    <button onClick={this.addList.bind(this)}>增加项目</button>
+                </div>
+                <ul>
+                   {
+                       this.state.list.map((item,index)=>{
+                           return (
+                               <li 
+                                key={index+item}
+                                onClick={this.deleteItem.bind(this,index)}
+                                >                                   
+                                {item}
+                                </li>
+                           )
+                       })
+                   }
+                </ul>
+            </Fragment>
+        )
+    }
+    inputChange(e){
+        //console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+    //增加列表
+    addList(){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
+    }
+    deleteItem(index){
+        console.log(index)
+        let list = this.state.list
+        list.splice(index,1)
+        this.setState({
+            list:list
+        })
+    }
+}
+export default Xiaojiejie
